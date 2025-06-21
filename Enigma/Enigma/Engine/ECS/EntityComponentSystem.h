ea4cbadf751ea3efed6e6d16776be322a7daf92f
MicroMcSkittles <noah.l.beauchamp@gmail.com>
2025-06-20 22:13:22 -0500

    Linux fix #5 I think

--- Enigma/Enigma/Engine/ECS/EntityComponentSystem.h
+++ Enigma/Enigma/Engine/ECS/EntityComponentSystem.h
@@ -24,25 +24,25 @@ namespace Enigma {
 
 				template<typename T>
 				ComponentPool<T>* GetPool() {
-					LOG_ASSERT(!m_Pools.count(typename T::GetType()), "No component pool of type %s exists", typename T::GetName());
-					return (ComponentPool<T>*)m_Pools[typename T::GetType()];
+					LOG_ASSERT(!m_Pools.count(T::GetType()), "No component pool of type %s exists", T::GetName());
+					return (ComponentPool<T>*)m_Pools[T::GetType()];
 				}
 				template<typename T>
 				Core::ID AddComponent(Core::ID entity) {
-					LOG_ASSERT(m_Entities.Get(entity).components.count(typename T::GetType()), "Entity already has a %s component", typename T::GetName());
-					if (!m_Pools.count(typename T::GetType()))
-						m_Pools.insert({ typename T::GetType(), new ComponentPool<T>() });
+					LOG_ASSERT(m_Entities.Get(entity).components.count(T::GetType()), "Entity already has a %s component", T::GetName());
+					if (!m_Pools.count(T::GetType()))
+						m_Pools.insert({ T::GetType(), new ComponentPool<T>() });
 					T comp;
 					comp.parentEntity = entity;
-					Core::ID compID = ((ComponentPool<T>*)m_Pools[typename T::GetType()])->Add(comp);
-					m_Entities.Get(entity).components.insert({ typename T::GetType(), compID });
+					Core::ID compID = ((ComponentPool<T>*)m_Pools[T::GetType()])->Add(comp);
+					m_Entities.Get(entity).components.insert({ T::GetType(), compID });
 					return compID;
 				}
 				template<typename T>
 				T& GetComponent(Core::ID entity) {
 					ComponentPool<T>* pool = GetPool<T>();
-					LOG_ASSERT(!m_Entities.Get(entity).components.count(typename T::GetType()), "Entity does not have a %s component", typename T::GetName());
-					return pool->Get(m_Entities.Get(entity).components[typename T::GetType()]);
+					LOG_ASSERT(!m_Entities.Get(entity).components.count(T::GetType()), "Entity does not have a %s component", T::GetName());
+					return pool->Get(m_Entities.Get(entity).components[T::GetType()]);
 				}
 
 			private:
